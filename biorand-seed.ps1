7c2f70dad01dc2ac3e25cc29b1873e5ab9930b48
Laezor <32393101+Laezor@users.noreply.github.com>
2024-12-20 20:38:40 +0200

    Update biorand-seed.ps1

--- biorand-seed.ps1
+++ biorand-seed.ps1
@@ -109,7 +109,7 @@ function Login-Biorand {
     $response = Invoke-RestMethod -Uri "https://api.biorand.net/auth/signin" -Method POST -Headers @{ "Content-Type" = "application/json" } -Body (@{ email = $email; code = $code } | ConvertTo-Json -Depth 2)
 
     if ($response.token) {
-        $configPath = "$env:USERPROFILE/reseed-config.json"
+        $configPath = "./reseed-config.json"
         $config = if (Test-Path $configPath) {
             Get-Content $configPath | ConvertFrom-Json
         } else {
